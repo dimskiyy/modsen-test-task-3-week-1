1751402c604089adc4f1e3770efa4b90487f0c82
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-02-07 22:42:49 +0300

    Add BookCard

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const BookCard = () => {
+const BookCard = ({book, onClick}) => {
     return (
-        <div>
-            
+        <div className='book_card' onClick={()=>onClick(book)}>
+            <img src={book.img} alt="Book cover" />
+            <h3>{book.title}</h3>
+            <p>{book.category}</p>
+            <p>{book.authors?.join(", ")}</p>
         </div>
     );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,27 @@
 import React from "react";
+import BookCard from "../components/BookCard";
 import Filter from "../components/Filter";
 import Header from "../components/Header";
 
 const Home = () => {
+    const testData = {
+        title: "Hamlet",
+        img: "pic.jpg",
+        category: "poetry",
+        authors: ["Shakespeare"],
+    };
+
+    const testHandleClick = (book) => {
+        console.log("Clicked on book:", book.title);
+    };
+
     return (
         <div>
-            <Header/>
-            <Filter/>
+            <Header />
+            <Filter />
             <h1>Home page</h1>
+            <BookCard book={testData} onClick={testHandleClick} />
+            <BookCard book={testData} onClick={testHandleClick} />
         </div>
     );
 };
