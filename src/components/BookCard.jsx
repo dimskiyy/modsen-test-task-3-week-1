f9141a028aa441d26215de0cdfe51db9bf0ab6e4
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-02-08 18:53:21 +0300

    Rework BookCard
    - Add styles

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
+import "./BookCard.css";
 
-const BookCard = ({book, onClick}) => {
+const BookCard = ({ book }) => {
+    if (!book) {
+        return null;
+    }
     return (
-        <div className='book_card' onClick={()=>onClick(book)}>
-            <img src={book.img} alt="Book cover" />
-            <h3>{book.title}</h3>
-            <p>{book.category}</p>
-            <p>{book.authors?.join(", ")}</p>
+        <div className="book_card" key={book.id}>
+            <div>
+                <h2>{book.title}</h2>
+            </div>
+            <div>
+                <img src={book.image_url} alt="#" />
+            </div>
+            <div>
+                <p>{book.genres}</p>
+            </div>
+            <div>
+                <p>{book.authors}</p>
+            </div>
         </div>
     );
 };
