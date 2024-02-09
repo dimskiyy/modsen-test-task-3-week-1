08d49e3092adb4cec15700ae6f66c6899ee730a9
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-02-10 00:03:01 +0300

    Add searching counter

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -6,6 +6,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const BookList = ({ search, category, sorting }) => {
     const [books, setBooks] = useState([]);
+    const [totalItems, setTotalItems] = useState(0);
 
     useEffect(() => {
         axios
@@ -16,31 +17,39 @@ const BookList = ({ search, category, sorting }) => {
                 let filteredBooks = res.data.items;
 
                 if (category !== "all") {
-                    filteredBooks = filteredBooks.filter(book =>
-                        book.volumeInfo.categories && book.volumeInfo.categories.map(cat => cat.toLowerCase()).includes(category.toLowerCase())
+                    filteredBooks = filteredBooks.filter(
+                        (book) =>
+                            book.volumeInfo.categories &&
+                            book.volumeInfo.categories
+                                .map((cat) => cat.toLowerCase())
+                                .includes(category.toLowerCase())
                     );
                 }
 
                 setBooks(filteredBooks);
+                setTotalItems(res.data.totalItems);
             })
             .catch((err) => console.log(err));
     }, [search, category, sorting]);
 
     return (
-        <div className="book_list">
-            {books.map((book) => (
-                <BookCard
-                    key={book.id}
-                    id={book.id}
-                    img={
-                        book.volumeInfo.imageLinks?.thumbnail ||
-                        "https://via.placeholder.com/180x250/566273/FFFFFF?text=no%20image"
-                    }
-                    title={book.volumeInfo.title}
-                    authors={book.volumeInfo.authors}
-                    category={book.volumeInfo.categories}
-                />
-            ))}
+        <div>
+            <p>Found {totalItems} books</p>
+            <div className="book_list">
+                {books.map((book) => (
+                    <BookCard
+                        key={book.id}
+                        id={book.id}
+                        img={
+                            book.volumeInfo.imageLinks?.thumbnail ||
+                            "https://via.placeholder.com/180x250/566273/FFFFFF?text=no%20image"
+                        }
+                        title={book.volumeInfo.title}
+                        authors={book.volumeInfo.authors}
+                        category={book.volumeInfo.categories}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
