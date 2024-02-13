bff699cae60408261c977e5a3df26d29b81aaa22
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-02-14 00:24:57 +0300

    Rework 404 page
    - feat: use MUI for page

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './NotFound.css';
+import React, { useEffect } from "react";
+import { Box, Button, Typography } from "@mui/material";
 
-export default function NotFound() {
-  const navigate = useNavigate();
+const NotFound = () => {
+    useEffect(() => {
+        document.title = "Page not found";
+    }, []);
 
-  useEffect(() => {
-    document.title = 'Page not found';
-  }, []);
-
-  const handleNavigate = () => {
-    navigate('/');
-    document.title = 'Home Page';
-  };
-
-  return (
-    <div className="notfound">
-      <div className="notfound_frame">
-        <div className="notfound_book">
-          <div className="notfound_title">
-            <h4 className="notfound_info">Page not found</h4>
-            <p className="notfound_status">404</p>
-          </div>
-        </div>
-        <button className='btn_404' type="button" onClick={handleNavigate}>
-          Open Home Page
-        </button>
-      </div>
-    </div>
-  );
-}
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                gap: "64px",
+                minHeight: "100vh",
+                backgroundColor: "FFFFFF",
+            }}
+        >
+            <Typography variant="h1" fontSize="128px" textAlign="center">
+                404
+            </Typography>
+            <Typography variant="h1" textAlign="center" maxWidth="md">
+                The page you’re looking for doesn’t exist.
+            </Typography>
+            <Button variant="contained" size="large" href="/">
+                Back Home
+            </Button>
+        </Box>
+    );
+};
+export default NotFound;
