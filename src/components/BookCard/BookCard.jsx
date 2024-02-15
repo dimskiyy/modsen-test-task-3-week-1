b836854e26441b6ef9a286bf88f316622b9c03b3
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-02-15 10:56:21 +0300

    Little changes

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import "./BookCard.css";
 import { Link } from "react-router-dom";
 
-function BookCard(props) {
+const BookCard = (props) => {
     const { id, img, title, authors, category } = props;
-  
+
     return (
-      <Link to={`/Book/${id}`} className="book_card" data-id={id}>
-        <img src={img} className="card_img" alt={title} title={title}></img>
-        <div>
-          <h5 className="card_title">{title}</h5>
-          <h6 className="card_category">{category?.slice(0, 1)}</h6>
-          <p className="card_authors">{authors?.join(', ')}</p>
-        </div>
-      </Link>
+        <Link to={`/Book/${id}`} className="book_card" data-id={id}>
+            <img src={img} className="card_img" alt={title} title={title}></img>
+            <div>
+                <h5 className="card_title">{title}</h5>
+                <h6 className="card_category">{category?.slice(0, 1)}</h6>
+                <p className="card_authors">{authors?.join(", ")}</p>
+            </div>
+        </Link>
     );
-  }
+};
 
 export default BookCard;
