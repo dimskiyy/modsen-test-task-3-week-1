361f207a279df786ccdd933e1ed7f2d0f8b9d3d9
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-02-19 13:12:48 +0300

    - Add Api into single component

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,56 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import BookCard from "../BookCard/BookCard";
-import "./BookList.css";
+import React, { useState } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
-import "../styles/Loader.css";
+import BookCard from "../BookCard/BookCard";
 import LoadButton from "../LoadButton/LoadButton";
-
-// const API_KEY = process.env.REACT_APP_API_KEY;
-const API_KEY = "AIzaSyCIxIIcpTwWrV5HmCj_q4AWZRAqD7y6CFI";
+import useGoogleBookApi from "../API/useGoogleBookApi";
+import "./BookList.css";
+import "../styles/Loader.css";
 
 const BookList = ({ search, category, sorting }) => {
-    const [books, setBooks] = useState([]);
     const [startId, setStartId] = useState(0);
-    const [initialTotalItems, setInitialTotalItems] = useState(0);
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setLoading(true);
-
-        axios
-            .get(
-                `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${API_KEY}&maxResults=30&orderBy=${sorting}&startIndex=${startId}`
-            )
-            .then((res) => {
-                let filteredBooks = res.data.items;
-
-                if (category !== "all") {
-                    filteredBooks = filteredBooks.filter(
-                        (book) =>
-                            book.volumeInfo.categories &&
-                            book.volumeInfo.categories
-                                .map((cat) => cat.toLowerCase())
-                                .includes(category.toLowerCase())
-                    );
-
-                    if (startId === 0) {
-                        setInitialTotalItems(filteredBooks.length);
-                    }
-                } else {
-                    if (startId === 0) {
-                        setInitialTotalItems(res.data.totalItems);
-                    }
-                }
-
-                setBooks(filteredBooks);
-                setLoading(false);
-            })
-            .catch((err) => {
-                console.log(err);
-                setLoading(false);
-            });
-    }, [search, category, sorting, startId]);
+    const { books, initialTotalItems, loading } = useGoogleBookApi(
+        search,
+        category,
+        sorting,
+        startId
+    );
 
     const handleLoadMore = () => {
         setStartId((prevStartIndex) => prevStartIndex + 30);
