e954ca14e441f9aac029f6f54d8f8f7ea1f792f3
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-02-26 12:57:44 +0300

    BookCard refactor

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -9,7 +9,7 @@ const BookCard = ({ id, img, title, authors, category }) => {
             <img src={img} className="card_img" alt={title} title={title}></img>
             <div>
                 <h5 className="card_title">{title}</h5>
-                <h6 className="card_category">{category?.slice(0, 1)}</h6>
+                <h6 className="card_category">{category?.[0]}</h6>
                 <p className="card_authors">{authors?.join(", ")}</p>
             </div>
         </Link>
