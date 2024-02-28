38ee3c3f6295c1b10c4861d7bc7b715739ade8cd
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-02-28 21:13:05 +0300

    Fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
-import { RoutesConfig } from "./constants/Routes";
-import NotFound from "./pages/404/NotFound";
-import Book from "./pages/book";
-import Home from "./pages/home";
+import { RoutesConfig } from "constants/Routes";
+import NotFound from "pages/404/NotFound";
+import Book from "pages/book";
+import Home from "pages/home";
 
 function App() {
     return (
