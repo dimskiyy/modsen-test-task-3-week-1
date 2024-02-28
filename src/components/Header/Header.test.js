590e32bc65cf19a2fa7cc1030f3c970ed53eef61
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-02-28 23:00:29 +0300

    Fix Header test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { fireEvent, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import "@testing-library/jest-dom/extend-expect";
 
