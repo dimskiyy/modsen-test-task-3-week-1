e2579cb4c2769cfe9ca91bba16b0e3e3590e4494
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-03-04 17:43:24 +0300

    Set global theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { RoutesConfig } from "constants/Routes";
 import NotFound from "pages/404/NotFound";
 import Book from "pages/book";
 import Home from "pages/home";
+import { mainTheme } from "themes/mainTheme";
 
 function App() {
     return (
-        <>
+        <ThemeProvider theme={mainTheme}>
+            <CssBaseline />
             <Routes>
                 <Route path={RoutesConfig.HOME} element={<Home />} />
                 <Route path={RoutesConfig.BOOK} element={<Book />} />
                 <Route path={RoutesConfig.NOT_FOUND} element={<NotFound />} />
             </Routes>
-        </>
+        </ThemeProvider>
     );
 }
 
---
+++ src/themes/mainTheme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+export const mainTheme = createTheme({
+    typography: {
+        fontFamily: "Quicksand, Arial",
+        fontWeightLight: 400,
+        fontWeightRegular: 500,
+        fontWeightMedium: 600,
+        fontWeightBold: 700,
+        fontSize: "2rem"
+    }
+});
