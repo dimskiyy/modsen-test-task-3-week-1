7f1b07adf2eeacd56518bc91535d8b62170dc9a8
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-03-04 14:53:12 +0300

    Style fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import "./Header.css";
 
 const Header = () => {
     return (
-        <div className="header header_wrapper">
+        <div className="header">
             <h1 className="header_title">
                 <Link className="header_logo" to={RoutesConfig.HOME}>
                     Search for books
