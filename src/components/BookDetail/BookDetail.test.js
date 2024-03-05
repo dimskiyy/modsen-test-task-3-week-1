a8a676ce16273fdfd68eb6662e5eb195005f36e7
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-03-05 22:12:15 +0300

    Test imports fix

--- src/components/BookDetail/BookDetail.test.js
+++ src/components/BookDetail/BookDetail.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { render, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
 import axios from "axios";
+
+import "@testing-library/jest-dom/extend-expect";
+
 import BookDetail from "./BookDetail";
 
 jest.mock("axios");
