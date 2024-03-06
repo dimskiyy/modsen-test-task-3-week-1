840e0a7dc298d154413a0d1b4c83d9dba850f460
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-03-06 12:09:23 +0300

    Add e2e test with WebDriverIO

---
+++ test/pageobjects/404.page.js
@@ -0,0 +1,25 @@
+const { browser } = require('@wdio/globals');
+
+class NotFoundPage {
+    open() {
+        return browser.url('/404');
+    }
+
+    get mainMessage() {
+        return $('//Typography[contains(text(), "The page you’re looking for doesn’t exist.")]');
+    }
+
+    get notFoundText() {
+        return $('//Typography[contains(text(), "404")]');
+    }
+
+    get backButton() {
+        return $('//Button[contains(text(), "Back Home")]');
+    }
+
+    clickBackHomeButton() {
+        this.backButton.click();
+    }
+}
+
+module.exports = new NotFoundPage();
---
+++ test/specs/test.e2e.js
@@ -0,0 +1,21 @@
+const { expect } = require('@wdio/globals')
+const NotFoundPage = require('../pageobjects/404.page')
+
+describe('NotFound Page', () => {
+    it('should display 404 message and Back Home button', () => {
+        NotFoundPage.open();
+
+        expect(NotFoundPage.mainMessage).toBeDisplayed();
+        expect(NotFoundPage.notFoundText).toBeDisplayed();
+        expect(NotFoundPage.backButton).toBeDisplayed();
+    });
+
+    it('clicking on Back Home button navigates to home page', () => {
+        NotFoundPage.open();
+
+        NotFoundPage.clickBackHomeButton();
+
+        expect(browser).toHaveUrl('/');
+    });
+});
+
