9fc72c9b1f78ebc456fff6dc6ede7bc17f1a8e69
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-03-11 20:07:53 +0300

    Add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { CssBaseline, IconButton, ThemeProvider } from "@mui/material";
 import { RoutesConfig } from "constants/Routes";
 import NotFound from "pages/404/NotFound";
 import Book from "pages/book";
 import Home from "pages/home";
+import { darkTheme } from "themes/darkTheme";
 import { mainTheme } from "themes/mainTheme";
 
 function App() {
+    const [theme, setTheme] = useState(mainTheme);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) =>
+            prevTheme === mainTheme ? darkTheme : mainTheme
+        );
+    };
+
     return (
-        <ThemeProvider theme={mainTheme}>
+        <ThemeProvider theme={theme}>
             <CssBaseline />
+            <IconButton onClick={toggleTheme}>
+                {theme === mainTheme ? (
+                    <Brightness4Icon />
+                ) : (
+                    <Brightness7Icon />
+                )}
+            </IconButton>
             <Routes>
                 <Route path={RoutesConfig.HOME} element={<Home />} />
                 <Route path={RoutesConfig.BOOK} element={<Book />} />
--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -6,18 +6,21 @@ import "./BookCard.css";
 
 const BookCard = memo(function BookCard({ id, img, title, authors, category }) {
     return (
-        <Link
-            to={`${RoutesConfig.BOOK_CARD}/${id}`}
-            className="book_card"
-            data-id={id}
-        >
-            <img src={img} className="card_img" alt={title} title={title}></img>
-            <div>
+        <div className="book_card">
+            <Link to={`${RoutesConfig.BOOK_CARD}/${id}`} data-id={id}>
+                <img
+                    src={img}
+                    className="card_img"
+                    alt={title}
+                    title={title}
+                ></img>
+            </Link>
+            <>
                 <h5 className="card_title">{title}</h5>
                 <h6 className="card_category">{category?.[0]}</h6>
                 <p className="card_authors">{authors?.join(", ")}</p>
-            </div>
-        </Link>
+            </>
+        </div>
     );
 });
 
