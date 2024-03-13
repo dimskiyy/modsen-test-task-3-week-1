ece7fe6a466634fe9004fef0d7a63b8b0c9a7f62
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-03-13 20:16:04 +0300

    Rework BookCard according to new features

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,25 +1,47 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { RoutesConfig } from "constants/Routes";
+import { useAppContext } from "context/appContext";
 
 import "./BookCard.css";
 
 const BookCard = memo(function BookCard({ id, img, title, authors, category }) {
+    const { favorites, addToFavorite, removeToFavorite } = useAppContext();
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    useEffect(() => {
+        setIsFavorite(favorites.some((book) => book.id === id));
+    }, [favorites, id]);
+
+    const toggleFavorite = () => {
+        if (isFavorite) {
+            removeToFavorite(id);
+        } else {
+            addToFavorite({ id, img, title, authors, category });
+        }
+        setIsFavorite(!isFavorite);
+    };
     return (
         <div className="book_card">
             <Link to={`${RoutesConfig.BOOK_CARD}/${id}`} data-id={id}>
-                <img
-                    src={img}
-                    className="card_img"
-                    alt={title}
-                    title={title}
-                ></img>
+                <img src={img} className="card_img" alt={title} title={title} />
             </Link>
-            <>
-                <h5 className="card_title">{title}</h5>
-                <h6 className="card_category">{category?.[0]}</h6>
-                <p className="card_authors">{authors?.join(", ")}</p>
-            </>
+            {isFavorite ? (
+                <FavoriteIcon
+                    sx={{ position: "absolute" }}
+                    onClick={toggleFavorite}
+                />
+            ) : (
+                <FavoriteBorderIcon
+                    sx={{ position: "absolute" }}
+                    onClick={toggleFavorite}
+                />
+            )}
+            <h6 className="card_category">{category?.[0]}</h6>
+            <p className="card_authors">{authors?.join(", ")}</p>
+            <h5 className="card_title">{title}</h5>
         </div>
     );
 });
