a5df5091d8339feb8ec73ef4421fc8a1777fb0f7
dimskiyy <93205162+dimskiyy@users.noreply.github.com>
2024-03-13 20:01:12 +0300

    Add FavoriteList

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+import { useAppContext } from "context/appContext";
+
+import BookCard from "components/BookCard/BookCard";
 
 const FavouriteList = () => {
+    const { favorites } = useAppContext();
+    const [favoriteBooks, setFavoriteBooks] = useState([]);
+
+    useEffect(() => {
+        setFavoriteBooks(favorites);
+    }, [favorites]);
+
     return (
         <div>
-            <h2>Избранные книги</h2>
+            <Typography variant="h2" textAlign="center" m={4}>
+                Favorite books
+            </Typography>
+            {favoriteBooks.length === 0 ? (
+                <Typography textAlign="center" variant="h4">
+                    The list of favorite books is empty
+                </Typography>
+            ) : (
+                <div className="book_list">
+                    {favoriteBooks.map((book) => (
+                        <BookCard
+                            key={book.id}
+                            id={book.id}
+                            img={book.img}
+                            title={book.title}
+                            authors={book.authors}
+                            category={book.category}
+                        />
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
